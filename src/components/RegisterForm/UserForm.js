8c5d6ffa562e1ed79af73d7a10bcc19a2753bbdc
Érico Grehs <ericogrehs@gmail.com>
2020-09-07 11:59:46 -0300

    feat: send data and create stepper

--- src/components/RegisterForm/UserForm.js
+++ src/components/RegisterForm/UserForm.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 
 function UserForm({ onSendForm }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        onSendForm();
+        onSendForm({ email, senha });
       }}
     >
       <TextField
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         id="email"
         label="email"
         type="email"
@@ -19,6 +24,8 @@ function UserForm({ onSendForm }) {
         margin="normal"
       />
       <TextField
+        value={senha}
+        onChange={(e) => setSenha(e.target.value)}
         id="senha"
         label="senha"
         type="password"
